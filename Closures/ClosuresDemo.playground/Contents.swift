108b9ef4cd377c914ae20329630f7d72101f6fcb
dengfeng520 <deng_feng520@163.com>
2021-03-24 11:31:00 +0800

    update code

--- Closures/ClosuresDemo.playground/Contents.swift
+++ Closures/ClosuresDemo.playground/Contents.swift
@@ -6,10 +6,12 @@ func comparison(closure: () -> Void) {
     
 }
 
-let nameList = ["1","2","3","4","5"]
-nameList.map { (name) in
-    
+
+let strList = ["1","2","3","4","5"]
+let numList: [Int] = strList.map { (num) in
+    return Int(num) ?? 0
 }
+
 
 // MARK: - 逃逸闭包
 func requestServer(with URL: String,parameter: @escaping(AnyObject?, Error?) -> Void) {
@@ -77,9 +79,11 @@ DownLoadImageManager.sharedInstance.downLoadImageWithEscapingClosure(path: path)
     } else {
         guard let image = image else { return }
         print("图片下载完成，显示图片: \(image)")
+        let imageView = UIImageView(image: image)
+        imageView.layer.cornerRadius = 5
     }
 }
-   
+
 enum Course {
     case spacePhysics // 空间物理
     case nuclearPhysics // 原子核物理
@@ -152,6 +156,7 @@ func autoAndEscaping(_ name: @autoclosure @escaping() -> String) {
 
 autoAndEscaping("")
 
+
 // MARK: - inout
 func configStudent(_ fraction: inout Int) -> Int {
     return fraction + 30
@@ -183,4 +188,121 @@ func configFraction(_ fraction: inout Int) -> Int {
 
 var location = Location(lat: 37.33020, lon: -122.024348)
 configLocation(&location.lat)
-configFraction(&location.fraction)
+configLocation(&location.lon)
+
+// MARK: - closures捕获值
+
+class Demo: NSObject {
+    var test = String()
+}
+// 常量
+var index = 10086
+// 变量
+var number = 1008611
+// 引用类型
+let demo = Demo()
+var capturel = {
+    index = 10086 + 998525
+    number = 1008611 - 998525
+    demo.test = "block test"
+    print("index==========\(index)")
+    print("number==========\(number)")
+    print("demo.test==========\(demo.test)")
+}
+number = number + 1
+demo.test = "test"
+capturel()
+
+func makeIncrementer(_ amount: Int) -> () -> Int {
+    var total = 0
+    // 闭包
+    func incrementer() -> Int {
+        total = total + amount
+        return total
+    }
+    return incrementer
+}
+
+let incrementerTen = makeIncrementer(10)
+incrementerTen()
+incrementerTen()
+incrementerTen()
+let incrementerSix = makeIncrementer(6)
+incrementerSix()
+incrementerSix()
+incrementerTen()
+let alsoIncrementerTen = incrementerTen
+alsoIncrementerTen()
+
+// MARK: - Closure循环引用
+enum Answer {
+    case A
+    case B
+    case C
+    case D
+}
+
+class Student: CustomStringConvertible {
+    var name: String = String()
+    lazy var replyClosure: (Answer) -> Void = { _ in
+        print("replyClosure=============\(self)")
+    }
+    
+    init(name: String) {
+        self.name = name
+        print("==========Student init==========")
+    }
+    
+    deinit {
+        print("==========Student deinit==========")
+    }
+}
+
+extension Student {
+    var description: String {
+        return "<Student: \(name)>"
+    }
+}
+
+class Teacher {
+    var isRight: Bool = false
+    
+    init() {
+        print("==========Teacher init==========")
+        let student = Student(name: "John Appleseed")
+        student.replyClosure = { (answer: Answer) in
+            
+        }
+        //        let judgeClosure = { (answer: Answer) in
+        //            print("\(student.name) is \(answer)")
+        //        }
+        //        student.replyClosure = judgeClosure
+    }
+    
+    deinit {
+        print("==========Teacher deinit==========")
+    }
+}
+
+
+var student = Student(name: "John Appleseed")
+student.replyClosure(.B)
+
+class Role {
+    var name: String
+    // ...
+    lazy var action: () -> Void = {
+        print("\(self) takes action.")
+    }
+    init(_ name: String = "Foo") {
+        self.name = name
+        print("\(self) init")
+    }
+    
+    deinit {
+        print("\(self) deinit")
+    }
+}
+
+var boss = Role("boss")
+boss.action()
